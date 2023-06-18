583f4c023ef159a93eaefca9c2b4cbbba95e59fc
roie <roie058@gmail.com>
2023-06-18 09:49:53 +0300

    better prefetch

--- src/components/homepage/NewHero.tsx
+++ src/components/homepage/NewHero.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography, useMediaQuery } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 import RoundLogo from '../../../public/images/roundlogo.svg'
 import {Heebo} from 'next/font/google'
 import Image from 'next/image'
@@ -12,6 +12,7 @@ import { TFunction } from 'i18next'
 import { NewSesstion } from '@/pages/api/auth/signup'
 import { useQuery } from '@tanstack/react-query'
 import { getPlans } from '@/util/fetchers'
+import { queryClient } from '@/pages/_app'
 type Props = {t: TFunction<"home", undefined, "home">}
 
 
@@ -20,12 +21,20 @@ const heebo=Heebo({subsets:["latin","hebrew"]})
 const NewHero = ({t}: Props) => {
 const {locale}=useRouter()
 const {data:session}:{data:NewSesstion}=useSession()
- useQuery({queryKey:['plans'],queryFn:()=>getPlans(session),enabled:!!session})
 const router=useRouter()
 const isXl=useMediaQuery("(max-width:1350px)")
 const isLg=useMediaQuery("(max-width:900px)")
 const isSmall=useMediaQuery("(max-width:600px)")
 
+
+const prefetchTodos = async () => {
+  if(session)await queryClient.prefetchQuery({queryKey:['plans'],queryFn:()=>getPlans(session)})
+}
+useEffect(()=>{
+  if(session){
+    prefetchTodos()
+  }
+},[session])
 
 
   return (
@@ -44,16 +53,16 @@ const isSmall=useMediaQuery("(max-width:600px)")
          <Box className={styles.circleGroup2}>
         <div ></div>
         <div></div>
-        <div ></div>
+        <div  ></div>
         </Box>
-    <Box className={styles.mainText}    >
+    <Box className={styles.mainText}     >
 <Typography  className={`${heebo.className} ${styles.header}`} fontWeight={"bold"} variant="h1" >{t('h1')}</Typography>
 <Image priority sizes='250px' className={styles.logo}  alt='travel buddy'  src={RoundLogo}   />
 </Box>
 <Typography textAlign={ locale=="he" ? "right" : "left"} className={`${heebo.className} ${styles.sub}`}>{t('sub1')} <span style={{color:"#238080"}}>{t("colorText1")}</span> {locale==='en'?"and":''} <span style={{color:"#0B799D"}}>{t("colorText2")}</span></Typography>
 </Box>
 
-<Box  paddingY={isSmall?"15%":"5%"} flexDirection={locale=='he'? "row-reverse" : "row"}  display={'flex'} alignItems={'center'} textAlign={'center'} flexWrap={"wrap"} justifyContent={"center"} gap={"15px"} maxWidth={'1000px'} >
+<Box paddingY={isSmall?"15%":"5%"} flexDirection={locale=='he'? "row-reverse" : "row"}  display={'flex'} alignItems={'center'} textAlign={'center'} flexWrap={"wrap"} justifyContent={"center"} gap={"15px"} maxWidth={'1000px'} >
  <Typography className={heebo.className} fontWeight={"bold"} variant='h2' fontSize={isXl?"2rem": "3rem"}>{t("startLink")}</Typography>
 {session? <UiButton style={{height:'50px',width:'200px',padding:'0'}} size='small' clickFn={()=>{router.push('/newplan')}}>{t("button")}</UiButton> :<UiButton style={{height:'50px',width:'200px',padding:'0'}} size='small' color='blue' clickFn={()=>router.push('/auth')}>{t("loginButton")}</UiButton>}
 </Box>
